3a5c3e5ee304e4467518a37151f414baafac5809
haolin <topazur@163.com>
2023-11-30 21:00:36 +0800

    docs(TagInput): update(#2625)

--- src/cascader/type.ts
+++ src/cascader/type.ts
@@ -35,7 +35,7 @@ export interface TdCascaderProps<CascaderOption extends TreeOptionData = TreeOpt
    */
   clearable?: boolean;
   /**
-   * 多选情况下，用于设置折叠项内容，默认为 `+N`。如果需要悬浮就显示其他内容，可以使用 collapsedItems 自定义。`value` 表示当前存在的所有标签，`onClose` 表示关闭标签时触发的事件s
+   * 多选情况下，用于设置折叠项内容，默认为 `+N`。如果需要悬浮就显示其他内容，可以使用 collapsedItems 自定义。`value` 表示当前存在的所有标签，`onClose` 表示关闭标签时触发的事件
    */
   collapsedItems?: TNode<{
     value: CascaderOption[];
